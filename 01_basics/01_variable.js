01832f00efe7aa22b1329ebe77d429cd67bd1dec
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2024-06-29 05:42:10 +0000

    variable file

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 14523
+let accountEmail = "utsavvachhani.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+
+// accountId = 2 //not allowed
+// console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// changeable the variable
+accountEmail = "lathiya.com"
+accountPassword = "20052005"
+accountCity = "Bhavnagar"
+console.table([accountId,accountEmail,accountPassword,accountCity])
