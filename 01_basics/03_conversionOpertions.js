4cfd2c07df99744f95a4e0ab67105912f13949cb
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2024-06-29 06:15:32 +0000

    conversion of opertion

---
+++ 01_basics/03_conversionOpertions.js
@@ -0,0 +1,25 @@
+let score = "33";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// Notes
+// "33" => 33
+// "33abc" => NaN
+// true =>1 false => 0
+
+let isLoggedIn = "utsav" ;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 =>  true; 0 => false ;
+// "" => false; "utsav" => true;
+
+let someNumber = 21
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber));
+console.log(stringNumber);
