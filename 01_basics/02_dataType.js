e611e71782513f816c559fb0a1030fed6728d24f
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2024-06-29 05:57:52 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as never version
+
+// documentastion of JS is avalible at tc39.es  or MDN
+
+let name = "utsav" //string
+let age = 19       //integer
+let isLoggedIn = false   //boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+//  undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null);
