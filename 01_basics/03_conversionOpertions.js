9bc540c81fbf448927e172fea8d197d0bb8b0ec3
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2024-06-29 06:37:14 +0000

    change in 03_conversionOpertions

--- 01_basics/03_conversionOpertions.js
+++ 01_basics/03_conversionOpertions.js
@@ -1,10 +1,10 @@
 let score = "33";
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // Notes
@@ -14,12 +14,39 @@ console.log(valueInNumber);
 
 let isLoggedIn = "utsav" ;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 =>  true; 0 => false ;
 // "" => false; "utsav" => true;
 
 let someNumber = 21
 let stringNumber = String(someNumber)
-console.log(typeof(stringNumber));
-console.log(stringNumber);
+// console.log(typeof(stringNumber));
+// console.log(stringNumber);
+
+
+// ****************** Opertions ****************************
+
+let value = 33 ; 
+let negValue = -value ;
+// console.log(negValue);
+                                                                                                                                                                                                                                                                               
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// // console.log(2**2); //=>power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " Utsav";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+// link to study 
+// https://tc39.es/ecma262/#sec-type-conversion
