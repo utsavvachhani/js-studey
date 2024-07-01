17e31a2c84acf189e47588eadb10d92c36d12e45
utsav_vachhani(uv) <149605853+utsavvachhani@users.noreply.github.com>
2024-07-01 07:48:13 +0000

    dataTypes updates

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,3 +5,16 @@
 // console.log(2 > 2);
 // console.log(2 < 2);
 
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);   //false
+// console.log(null == 0);  //false
+// console.log(null >= 0);  //true
+
+// console.log(undefined > 0);
+// console.log(undefined > 0);
+// console.log(undefined > 0);
+
+// ===
+console.log("2" === 2);
